cf1e99041548428e38938bc83e118ea85174b4e7
vzmultiship <vzmultiship@yandex-team.ru>
2016-03-25 13:40:20 +0300

    anagram fix

--- fun/anagram.py
+++ fun/anagram.py
@@ -1,7 +1,7 @@
 def isAnagram(s1, s2):
 	l1 = list(s1)
 	l2 = list(s2)
-	match = [s for s in l1 if s in l2]
+	match = [(el) for el in l1 if el in l2]
 	if(len(l1) != len(l2) or len(match) == 0 or len(match) != len(l1)):
 		return False
 	return True
